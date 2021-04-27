c4bd84578d5aeed16ad50cd8e2b0bc4bdafa8e74
goellner <stefan@goellner.io>
2021-04-27 17:18:06 +0200

    fix: config should be published automatically now

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -12,7 +12,19 @@ class ServiceProvider extends AddonServiceProvider
 	public function boot()
 	{
 		parent::boot();
+
+		$this->handleConfig();
 	}
+
+	protected function handleConfig()
+	{
+		$this->mergeConfigFrom(__DIR__ . '/../config/statamic-image-renderer.php', 'statamic-image-renderer');
+
+		$this->publishes([
+			__DIR__ . '/../config/statamic-image-renderer.php' => config_path('statamic-image-renderer.php'),
+		], 'statamic-image-renderer-config');
+	}
+
 
 	protected $commands = [
 		GenerateBlurhashStrings::class,
